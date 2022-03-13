cd2a68d7e768b20fa7a7c3014edb5815e04a3533
Александр Кушелев <a.kushelev@yandex.ru>
2022-03-13 22:34:21 +0400

    Some changes 2

---
+++ yatube/posts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    slug = models.SlugField(unique=True, verbose_name='Адрес')
+    description = models.TextField(blank=False, verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(blank=False, verbose_name='Описание')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='groups',
+        verbose_name='Группа'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+
+    def __str__(self):
+        return self.text
