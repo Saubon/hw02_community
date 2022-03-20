9158d6c3951fd3311d53ce8d0c90cd3f62eb90c5
Александр Кушелев <a.kushelev@yandex.ru>
2022-03-20 23:19:52 +0400

    Admin.py edited2

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -14,7 +14,7 @@ class PostAdmin(admin.ModelAdmin):
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'slug', 'description')
     list_editable = ('title','description',)
-    search_fields = ('text',)
+    search_fields = ('title',)
     list_filter = ('title',)
     empty_value_display = '-пусто-'
 
