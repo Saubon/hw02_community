0865b18044fe8370d8723194968a1278e0f2a61d
Александр Кушелев <a.kushelev@yandex.ru>
2022-03-20 23:35:34 +0400

    For Review 3

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,12 +1,12 @@
-#from django.contrib.auth.views import LogoutView 
-#from django.urls import path
+#  from django.contrib.auth.views import LogoutView 
+#  from django.urls import path
 
-#app_name = 'users'
+#  app_name = 'users'
 
-#urlpatterns = [
-    #path(
-      #'logout/',
-      #LogoutView.as_view(template_name='users/logged_out.html'),
-      #name='logout'
-    #),
-#]
+#  urlpatterns = [
+    #  path(
+      #  'logout/',
+      #  LogoutView.as_view(template_name='users/logged_out.html'),
+      #  name='logout'
+    #  ),
+#  ]
