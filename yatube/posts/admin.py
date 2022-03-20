54ac8afdc4faa5a8ab7b056463d23e2fff765c0d
Александр Кушелев <a.kushelev@yandex.ru>
2022-03-20 23:19:07 +0400

    Admin.py - GroupAdmin edited

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -12,12 +12,12 @@ class PostAdmin(admin.ModelAdmin):
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
-    list_editable = ('group/<slug:slug>',)
+    list_display = ('pk', 'title', 'slug', 'description')
+    list_editable = ('title','description',)
     search_fields = ('text',)
-    list_filter = ('pub_date',)
+    list_filter = ('title',)
     empty_value_display = '-пусто-'
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
