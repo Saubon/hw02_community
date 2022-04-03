72286fc1fbd0fde20aa13d1b7d104bf8a24768c8
Александр Кушелев <a.kushelev@yandex.ru>
2022-04-03 20:19:33 +0400

    Auth urls fixed (5/6), except password_change/done

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -18,32 +18,32 @@ urlpatterns = [
         name='login'
     ),
     path(
-        'auth/password_change/',
+        'password_change/',
         PasswordChangeView.as_view(template_name='users/password_change_form.html'),
         name='pass_change_view'
     ),
     path(
-        'auth/password_change/done/',
+        'password_change/done/',
         PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
         name='pass_change_done'
     ),
     path(
-        'auth/password_reset',
+        'password_reset/',
         PasswordResetView.as_view(template_name='users/password_reset_form.html'),
         name='pass_reset'
     ),
     path(
-        'auth/password_reset/done/',
+        'password_reset/done/',
         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
         name='pass_reset_done'
     ),
     path(
-        'auth/reset/<uidb64>/token/',
+        'reset/<uidb64>/token/',
         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
         name='pass_reset_confirm'
     ),
     path(
-        'auth/reset/done/',
+        'reset/done/',
         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
         name='pass_reset_complete'
     ),
