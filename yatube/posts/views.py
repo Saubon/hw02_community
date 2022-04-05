706415400bd9c83b5282cb620b17933354f12e41
Александр Кушелев <a.kushelev@yandex.ru>
2022-04-06 00:22:13 +0400

    Added Paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,39 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
 
-def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
-    context = {
-        'posts': posts,
+def get_page_context(queryset, request):
+    paginator = Paginator(queryset, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return {
+        'paginator': paginator,
+        'page_number': page_number,
+        'page_obj': page_obj,
     }
+
+def index(request):
+    context = get_page_context(Post.objects.all())
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:10]
     context = {
+        'group': group,
         'posts': posts,
-        'groups': group,
     }
+    context.update(get_page_context(group.posts.all(), request))
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    context = {
+        'author': author,
+    }
+    context.update(get_page_context(author.posts.all(), request))
+    return render(request, 'posts/profile.html', context)
